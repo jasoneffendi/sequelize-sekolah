de9d6e76e273e84b4d2315524720b5f0060a65c1
jasoneffendi <jasoneffendi27@gmail.com>
2017-09-12 16:42:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,6 @@ app.use('/students', students)
 // =================== end of routing  ========================== //
 
 //=========================================
-app.listen(proccess.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('app start on port 3000');
 })
